454c170ec1631ef0161c6908cb8807112ce088f2
lilu <lilu@liludeMacBook-Pro.local>
2019-02-21 17:16:56 +0800

    QIMKitVendor 2.0.1 配置打印log

--- QIMKitVendor/QIMPublicRedefineHeader/QIMPublicRedefineHeader.h
+++ QIMKitVendor/QIMPublicRedefineHeader/QIMPublicRedefineHeader.h
@@ -9,15 +9,29 @@
 #ifndef QIMPublicRedefineHeader_h
 #define QIMPublicRedefineHeader_h
 
-#import "CocoaLumberjack.h"
+#if defined (QIMLogEnable) && QIMLogEnable == 1
 
-static const DDLogLevel ddLogLevel = DDLogLevelVerbose;
-//是否开启日志，根据项目配置来
-#define NSLog(frmt, ...) DDLogVerbose(frmt, ##__VA_ARGS__)//版本信息为橙色
-#define QIMErrorLog(frmt, ...) DDLogError(frmt, ##__VA_ARGS__)//错误信息为红白
-#define QIMWarnLog(frmt, ...) DDLogWarn(frmt, ##__VA_ARGS__)//警告为黑黄
-#define QIMInfoLog(frmt, ...) DDLogInfo(frmt, ##__VA_ARGS__)//信息为蓝白
-#define QIMDebugLog(frmt, ...) DDLogDebug(frmt, ##__VA_ARGS__)//调试为白黑
-#define QIMVerboseLog(frmt, ...) DDLogVerbose(frmt, ##__VA_ARGS__)//版本信息为橙色
+    #import "CocoaLumberjack.h"
+
+    static const DDLogLevel ddLogLevel = DDLogLevelVerbose;
+    //是否开启日志，根据项目配置来
+    #define NSLog(frmt, ...) DDLogVerbose(frmt, ##__VA_ARGS__)//版本信息为橙色
+    #define QIMErrorLog(frmt, ...) DDLogError(frmt, ##__VA_ARGS__)//错误信息为红白
+    #define QIMWarnLog(frmt, ...) DDLogWarn(frmt, ##__VA_ARGS__)//警告为黑黄
+    #define QIMInfoLog(frmt, ...) DDLogInfo(frmt, ##__VA_ARGS__)//信息为蓝白
+    #define QIMDebugLog(frmt, ...) DDLogDebug(frmt, ##__VA_ARGS__)//调试为白黑
+    #define QIMVerboseLog(frmt, ...) DDLogVerbose(frmt, ##__VA_ARGS__)//版本信息为橙色
+
+#else
+
+    //是否开启日志，根据项目配置来
+    #define NSLog(frmt, ...)
+    #define QIMErrorLog(frmt, ...)
+    #define QIMWarnLog(frmt, ...)
+    #define QIMInfoLog(frmt, ...)
+    #define QIMDebugLog(frmt, ...)
+    #define QIMVerboseLog(frmt, ...) 
+
+#endif
 
 #endif /* QIMPublicRedefineHeader_h */
